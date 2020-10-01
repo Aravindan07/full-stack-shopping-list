ffd0bcbd41b735cde5d4bc23bc771f54a0357853
Aravindan07 <50622300+Aravindan07@users.noreply.github.com>
2020-10-01 15:07:59 +0530

    Version 1.1.1

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MongoURI:
+        'mongodb+srv://aravindDb:aravindDbPassword@mern-stack-application.fbaao.mongodb.net/mern-stack-application?retryWrites=true&w=majority',
+};
